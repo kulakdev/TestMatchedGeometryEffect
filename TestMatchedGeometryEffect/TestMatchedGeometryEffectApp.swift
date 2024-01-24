4b1165e047d5b0ea515c4833b6bbc6747b180021
kulakdev <constantinekulak@icloud.com>
2024-01-25 00:14:19 +0200

    Initial Commit

---
+++ TestMatchedGeometryEffect/TestMatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMatchedGeometryEffectApp.swift
+//  TestMatchedGeometryEffect
+//
+//  Created by Macbook Pro on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestMatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
